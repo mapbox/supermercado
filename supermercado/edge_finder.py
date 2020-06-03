84c357f4651d02a813e7be006a5b50e2aaaa8506
dnomadb <damon@mapbox.com>
2020-06-03 14:24:16 -0700

    clear up namespace

--- supermercado/edge_finder.py
+++ supermercado/edge_finder.py
@@ -4,8 +4,8 @@ import numpy as np
 
 from supermercado import super_utils as sutils
 
-def findedges(inputtiles, parsenames):
 
+def findedges(inputtiles, parsenames):
 
     tiles = sutils.tile_parser(inputtiles, parsenames)
 
@@ -21,9 +21,12 @@ def findedges(inputtiles, parsenames):
     idxs = sutils.get_idx()
 
     # Using the indices to roll + stack the array, find the minimum along the rolled / stacked axis
-    xys_edge = (np.min(np.dstack((
-        np.roll(np.roll(burn, i[0], 0), i[1], 1) for i in idxs
-        )), axis=2) - burn)
+    xys_edge = (
+        np.min(
+            np.dstack([np.roll(np.roll(burn, i[0], 0), i[1], 1) for i in idxs]), axis=2
+        )
+        ^ burn
+    )
 
     # Set missed non-tiles to False
     xys_edge[burn == False] = False
@@ -35,8 +38,10 @@ def findedges(inputtiles, parsenames):
 
     # Return the edge array
 
-    return np.append(xys_edge, np.zeros((xys_edge.shape[0], 1), dtype=np.uint8) + zoom, axis=1)
+    return np.append(
+        xys_edge, np.zeros((xys_edge.shape[0], 1), dtype=np.uint8) + zoom, axis=1
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     findedges()
--- supermercado/super_utils.py
+++ supermercado/super_utils.py
@@ -2,20 +2,25 @@ import numpy as np
 
 import json, re, mercantile
 
+
 def parseString(tilestring, matcher):
-    tile = [int(r) for r in matcher.match(tilestring).group().split('-')]
+    tile = [int(r) for r in matcher.match(tilestring).group().split("-")]
     tile.append(tile.pop(0))
     return tile
+
 
 def get_range(xyz):
     return xyz[:, 0].min(), xyz[:, 0].max(), xyz[:, 1].min(), xyz[:, 1].max()
 
+
 def burnXYZs(tiles, xmin, xmax, ymin, ymax, pad=1):
     # make an array of shape (xrange + 3, yrange + 3)
-    burn = np.zeros((xmax - xmin + (pad * 2 + 1), ymax - ymin + (pad * 2 + 1)), dtype=bool)
+    burn = np.zeros(
+        (xmax - xmin + (pad * 2 + 1), ymax - ymin + (pad * 2 + 1)), dtype=bool
+    )
 
     # using the tile xys as indicides, burn in True where a tile exists
-    burn[(tiles[:,0] - xmin + pad, tiles[:, 1] - ymin + pad)] = True
+    burn[(tiles[:, 0] - xmin + pad, tiles[:, 1] - ymin + pad)] = True
 
     return burn
 
@@ -29,11 +34,13 @@ def tile_parser(tiles, parsenames=False):
 
     return tiles
 
+
 def get_idx():
     tt = np.zeros((3, 3), dtype=bool)
     tt[1, 1] = True
 
     return np.dstack(np.where(tt != True))[0] - 1
+
 
 def get_zoom(tiles):
     t, d = tiles.shape
@@ -45,19 +52,26 @@ def get_zoom(tiles):
 
     return tiles[0, 2]
 
+
 def filter_polygons(features):
     for f in features:
-        if 'geometry' in f and 'type' in f['geometry']:
-            if f['geometry']['type'] == 'Polygon':
+        if "geometry" in f and "type" in f["geometry"]:
+            if f["geometry"]["type"] == "Polygon":
                 yield f
-            elif f['geometry']['type'] == 'MultiPolygon':
-                for part in f['geometry']['coordinates']:
-                    yield {
-                        'geometry': {
-                            'type': 'Polygon',
-                            'coordinates': part
-                        }
-                    }
+            elif f["geometry"]["type"] == "Point":
+                yield f
+            elif f["geometry"]["type"] == "LineString":
+                yield f
+            elif f["geometry"]["type"] == "MultiPolygon":
+                for part in f["geometry"]["coordinates"]:
+                    yield {"geometry": {"type": "Polygon", "coordinates": part}}
+            elif f["geometry"]["type"] == "MultiPoint":
+                for part in f["geometry"]["coordinates"]:
+                    yield {"geometry": {"type": "Point", "coordinates": part}}
+            elif f["geometry"]["type"] == "MultiLineString":
+                for part in f["geometry"]["coordinates"]:
+                    yield {"geometry": {"type": "LineString", "coordinates": part}}
+
 
 class Unprojecter:
     def __init__(self):
@@ -67,11 +81,14 @@ class Unprojecter:
     def xy_to_lng_lat(self, coordinates):
         for c in coordinates:
             tc = np.array(c)
-            yield np.dstack([
-                tc[:, 0] * self.R2D / self.A,
-                ((np.pi * 0.5) - 2.0 * np.arctan(np.exp(-tc[:, 1] / self.A))) * self.R2D
-                ])[0].tolist()
+            yield np.dstack(
+                [
+                    tc[:, 0] * self.R2D / self.A,
+                    ((np.pi * 0.5) - 2.0 * np.arctan(np.exp(-tc[:, 1] / self.A)))
+                    * self.R2D,
+                ]
+            )[0].tolist()
 
     def unproject(self, feature):
-        feature['coordinates'] = [f for f in self.xy_to_lng_lat(feature['coordinates'])]
+        feature["coordinates"] = [f for f in self.xy_to_lng_lat(feature["coordinates"])]
         return feature
