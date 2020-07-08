a1d02b476ec1d903bbacb0eee218b464ead46292
dnomadb <damon@mapbox.com>
2020-07-08 14:25:33 -0700

    line test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -64,6 +64,25 @@ def test_burn_tile_center_point_roundtrip():
     assert json.loads(result.output) == tile
 
 
+def test_burn_tile_center_lines_roundtrip():
+    tiles = list(mercantile.children([0, 0, 0]))
+    bounds = (mercantile.bounds(*t) for t in tiles)
+    coords = (((e - w) / 2 + w, (n - s) / 2 + s) for w, s, e, n in bounds)
+
+    features = {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {"type": "LineString", "coordinates": list(coords)}
+    }
+
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["burn", "1"], input=json.dumps(features))
+
+    output_tiles = [json.loads(t) for t in result.output.split("\n") if t]
+    assert sorted(output_tiles) == sorted([list(t) for t in tiles])
+
+
 def test_burn_cli_tile_shape():
     tilegeom = '{"bbox": [-122.4755859375, 37.75334401310657, -122.431640625, 37.78808138412046], "geometry": {"coordinates": [[[-122.4755859375, 37.75334401310657], [-122.4755859375, 37.78808138412046], [-122.431640625, 37.78808138412046], [-122.431640625, 37.75334401310657], [-122.4755859375, 37.75334401310657]]], "type": "Polygon"}, "id": "(1309, 3166, 13)", "properties": {"title": "XYZ tile (1309, 3166, 13)"}, "type": "Feature"}'
     runner = CliRunner()
