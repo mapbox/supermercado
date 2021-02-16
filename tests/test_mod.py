6958ab89288a49d5fb653f99462997a25a16e8d6
dnomadb <damon@mapbox.com>
2021-02-16 12:43:03 -0800

    handling for properties

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -188,6 +188,30 @@ def test_filter_features_multi_point():
     assert list(sutils.filter_features(features)) == expected
 
 
+def test_filter_features_multi_point_properties():
+    """MultiPoints should be turned into multiple Points"""
+    features = [
+        {
+            "type": "Feature",
+            "geometry": {"type": "MultiPoint", "coordinates": [[0, 0], [1, 0]]},
+            "properties": {"a": "property"},
+        }
+    ]
+    expected = [
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [0, 0]},
+            "properties": {"a": "property"},
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [1, 0]},
+            "properties": {"a": "property"},
+        },
+    ]
+    assert list(sutils.filter_features(features)) == expected
+
+
 def test_filter_features_multi_linstrings():
     """MultiLineStrings should be turned into multiple LineStrings"""
     features = [
@@ -216,6 +240,42 @@ def test_filter_features_multi_linstrings():
                 "type": "LineString",
                 "coordinates": [[0, 0], [1, 0], [1, 1], [0, 1], [0, 1]],
             },
+        },
+    ]
+    assert list(sutils.filter_features(features)) == expected
+
+
+def test_filter_features_multi_linstrings_properties():
+    """MultiLineStrings should be turned into multiple LineStrings"""
+    features = [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "MultiLineString",
+                "coordinates": [
+                    [[0, 0], [1, 0], [1, 1], [0, 1], [0, 1]],
+                    [[0, 0], [1, 0], [1, 1], [0, 1], [0, 1]],
+                ],
+            },
+            "properties": {"a": "property"},
+        }
+    ]
+    expected = [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [[0, 0], [1, 0], [1, 1], [0, 1], [0, 1]],
+            },
+            "properties": {"a": "property"},
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [[0, 0], [1, 0], [1, 1], [0, 1], [0, 1]],
+            },
+            "properties": {"a": "property"},
         },
     ]
     assert list(sutils.filter_features(features)) == expected
